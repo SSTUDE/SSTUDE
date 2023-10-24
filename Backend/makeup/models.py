c727cb8cb2c09a4d7a836f378c0fd8754f28f906
surimjeon <gds0813@naver.com>
2023-10-24 16:11:44 +0900

    ✨ feat : domain모델 정의 | 전수림 | makeup

--- Backend/makeup/models.py
+++ Backend/makeup/models.py
@@ -9,6 +9,11 @@ class PersonalColor(Base):
     __tablename__ = "color"
 
     id = Column(BIGINT, nullable=False, autoincrement=True, primary_key=True)
-    name = Column(TEXT, nullable=False)
-    number = Column(INT, nullable=False)
+    accountId = Column(BIGINT, nullable=False) # account 어떻게 join???
+    colorName = Column(TEXT, nullable=False) #16진수 형태의 값 
+    matchColor = Column(INT, nullable=False)
+    hairColor = Column(TEXT, nullable=False)
+    accessary = Column(TEXT, nullable=False) 
+    explain = Column(TEXT, nullable=False)
+    
  
