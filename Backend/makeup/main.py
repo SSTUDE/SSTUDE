fe93399330df2273541eb045406e827c9e5db25c
surimjeon <gds0813@naver.com>
2023-10-26 10:36:16 +0900

    ✨ feat : route 구조로 변경 | 전수림 | makeup

--- Backend/makeup/main.py
+++ Backend/makeup/main.py
@@ -1,21 +1,19 @@
-from typing import Union
+from settings import engine
+from settings import metadata
+from settings import app
+import uvicorn
 
-from fastapi import FastAPI
+from route import post_route
 
-app = FastAPI()
+app.include_router(post_route, prefix="/api/post", tags=["post"])
 
 
-# 퍼스널컬러 요청 후 결과값을 db에 저장한 후 반환한다 
-@app.post("/makeup/color")
-def runColor():
-    return {"makeup": "color"}
+@app.get("/")
+def home():
+    return {"message": "Welcome to FastAPI CRUD Example."}
 
-# 퍼스널 컬러 기록을 반환한다 
-@app.get("/makeup/{record_id}")
-def getRecord (record_id: int, q: Union[str, None] = None):
-    return {"item_id": record_id, "q": q}
 
-# 화장 aapi를 요청받으면, ai가 실행되도록
-@app.get("/makeup/simulation")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+if __name__ == '__main__':
+    metadata.create_all(engine)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000)
+
