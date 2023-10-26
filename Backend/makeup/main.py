2cd5b197c1c9848c2797a766983f084e573a2f62
surimjeon <gds0813@naver.com>
2023-10-26 14:35:02 +0900

    🛠 fix : method 수정 | 전수림 | makeup

--- Backend/makeup/main.py
+++ Backend/makeup/main.py
@@ -1,19 +1,32 @@
-from settings import engine
-from settings import metadata
-from settings import app
-import uvicorn
 
-from route import post_route
+from fastapi import FastAPI, File, UploadFile, Form, Header
+from typing import Optional
 
-app.include_router(post_route, prefix="/api/post", tags=["post"])
+app = FastAPI()
 
 
-@app.get("/")
-def home():
-    return {"message": "Welcome to FastAPI CRUD Example."}
+# 퍼스널컬러 요청 후 결과값을 db에 저장한 후 반환한다 
+@app.post("/makeup/color")
+async def runColor(
+    file: UploadFile = File(), 
+    random_header: Optional[str] = Header(None, convert_underscores=False)
+):
+    
+    
+    return {
+        "file_size": file.filename,
+        "user_agent": random_header,
+    }
 
+    
+    
+# # 퍼스널 컬러 기록을 반환한다 
+# @app.get("/makeup/{record_id}")
+# def getRecord (record_id: int, q: Union[str, None] = None):
+#     return {"item_id": record_id, "q": q}
 
-if __name__ == '__main__':
-    metadata.create_all(engine)
-    uvicorn.run("main:app", host="127.0.0.1", port=8000)
+# # 화장 aapi를 요청받으면, ai가 실행되도록
+# @app.get("/makeup/simulation")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
 
