fff0034a6fef68e9f2f2fdae01b12a082acb41ed
surimjeon <gds0813@naver.com>
2023-10-30 17:56:44 +0900

    ✨ feat : 패션 점수 틀 만들기 & 통계 프로젝트 생성 | 전수림 | makeup

--- Backend/makeup/main.py
+++ Backend/makeup/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File, UploadFile, Header, HTTPException, status, Path
+from fastapi import FastAPI, File, UploadFile, Header, HTTPException, status
 from fastapi.responses import JSONResponse
 from typing import Optional
 from personal_color_analysis import personal_color
@@ -20,25 +20,25 @@ async def runColor(
     file: UploadFile = File(), 
     access_token: Optional[str] = Header(None, convert_underscores=False)
 ):
+    connect, curs = connectMySQL()
     # 헤더에 담긴 엑세스토큰을 spring으로 넘겨주고 받음 
     # userid = requests.post("http://k9d204.p.ssafy.io:8000/account/memberId", json={"accessToken": access_token}, headers={"Content-Type": "application/json"})
-    connect, curs = connectMySQL()
     userid=2
-    count = 11
-    
     
     ## 하루에 3번 이상 요청할 수 없음 
     current_date = datetime.now().date()
     with connect.cursor() as curs:
         query = """SELECT COUNT(*) AS request_count
                     FROM makeups
-                    WHERE member_id = %s AND calender = %s"""
+                    WHERE member_id = %s AND DATE_FORMAT(calender, '%%Y-%%m-%%d')= %s"""
         curs.execute(query, (userid, current_date))
         result = curs.fetchone()
         count = result[0] if result else 0
     
-    if count >=3:
-        raise HTTPException(status_code=429, detail="하루에 3번 이상 요청할 수 없습니다.")
+    
+    # 나중에 redis로 바꾸기!
+    if count >=1:
+        raise HTTPException(status_code=429, detail="하루에 1번 이상 요청할 수 없습니다.")
     
     else:
         try:
@@ -95,6 +95,88 @@ async def runColor(
                          'accessary': accessary, 'expl':expl,
                          'skin':skin, 'eye':eye})
     
+
+# 이미지 내에 사람이 서있으면, 사진에서 상의를 찾아서 색 추출 및 유사도 검사or점수
+# 무조건 바로 전 2개사진 뽑아서 주기
+@app.post("/makeup/clothes")
+async def read_item(file: UploadFile = File(), 
+            access_token: Optional[str] = Header(None, convert_underscores=False)):
+    connect, curs = connectMySQL()
+    # 헤더에 담긴 엑세스토큰을 spring으로 넘겨주고 받음 
+    # userid = requests.post("http://k9d204.p.ssafy.io:8000/account/memberId", json={"accessToken": access_token}, headers={"Content-Type": "application/json"})
+    userid=2
+    
+    current_date = datetime.now().date()
+    print(datetime.now())
+    with connect.cursor() as curs:
+        query = """SELECT COUNT(*) AS request_count
+                    FROM clothes
+                    WHERE member_id = %s AND calender = %s"""
+        curs.execute(query, (userid, current_date))
+        result = curs.fetchone()
+        count = result[0] if result else 0
+    
+    if count >=3:
+        raise HTTPException(status_code=429, detail="하루에 3번 이상 요청할 수 없습니다.")
+    
+    else:
+        try:
+            contents = await file.read()
+
+            #로컬에 파일 저장
+            file_name = 'savedclothfile.jpg'   
+            with open(file_name, "wb") as local_file:
+                local_file.write(contents)
+            uri = os.path.abspath('./savedclothfile.jpg')
+            
+            # S3저장 후 uri받아옴
+            s3uri = s3(file, userid, contents, count, current_date)
+            
+            
+            # 사진은 personalcolor을 판단하고, DB에 결과값을 저장한다 
+            result = personal_color.analysis(uri)
+            
+            print(result)
+            result = result.split('톤')[0]
+            score = changeId(result)
+            
+            # DB에 저장
+            with connect.cursor() as curs:
+                query = """INSERT INTO clothes (member_id, img_uri, score) VALUES (%s, %s, %s)"""
+                curs.execute(query, (userid, s3uri, score))
+            
+            connect.commit()
+            
+            with connect.cursor() as curs:
+            # 결과값, 사용자값 등을 모두 가져와서 JSON형태로 반환
+                query_select = """SELECT score, img_uri, MINUTE(calender) as minute
+                                FROM clothes
+                                WHERE member_id = %s AND DATE_FORMAT(calender, '%%Y-%%m-%%d')= %s
+                                LIMIT 2
+                                """
+                curs.execute(query_select,(userid, current_date))
+                row = curs.fetchall()
+                lst = []
+                for r in row:
+                    lst.append({'score': r[0], 'img_url': r[1], 'minute':r[2]})
+            
+            os.remove(file_name)
+    
+        except Exception as e:
+            print(e)
+            result = False
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="분석에 실패했습니다")
+        
+        finally:
+            connect.close()
+            
+    return JSONResponse(lst)
+
+    
+###########################################################################   
+    
+    
+    
     
 # # 퍼스널 컬러 이전 기록 리스트로 반환(달력) 
 # @app.post("/makeup/list")
@@ -146,9 +228,4 @@ async def runColor(
     
 #     return {"month": request.month, "day": request.day, "list":lst}
 
-
-# # 이미지 내에 사람이 서있으면, 사진에서 상의를 찾아서 색 추출 및 유사도 검사or점수
-# @app.get("/makeup/cloth")
-# def read_item(item_id: int, q: Union[str, None] = None):
-#     return {"item_id": item_id, "q": q}
 
