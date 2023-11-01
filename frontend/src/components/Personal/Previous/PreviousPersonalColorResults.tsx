aa159a0cb1136a8aa0cf2bd65a511b0a5b746cb7
안솔비 <caca30@naver.com>
2023-11-01 17:55:47 +0900

    🗑️ delete : 불필요한 파일 삭제  | 안솔비 | assets

--- frontend/src/components/Personal/Previous/PreviousPersonalColorResults.tsx
+++ frontend/src/components/Personal/Previous/PreviousPersonalColorResults.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { styled } from "styled-components";
-import images from "../../../constants/images";
+// import images from "../../../constants/images";
 
 const StyledContainer = styled.div`
   width: 100%;
