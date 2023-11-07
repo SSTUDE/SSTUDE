65f62290d4f51a383d0ae14636f57872b3f3b90d
김수찬 <tncks097@naver.com>
2023-11-07 09:46:30 +0900

    feat : 서비스 제공을 위한 ResponseDTO 설정 | 김수찬

---
+++ Backend/busstation/src/main/java/com/sstude/busstation/dto/response/BusResponseDto.java
@@ -0,0 +1,20 @@
+package com.sstude.busstation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BusResponseDto {
+    private Integer cityCode;
+    private String latitude;
+    private String longitude;
+    private String nodeId;
+    private String nodeName;
+    private Integer nodeNo;
+
+}
