ff40df78b4db94ff567571800d4511167f475f88
강민석 <wwwttt123@naver.com>
2023-11-07 09:57:54 +0900

    ✨ feat : 저녁 하늘 아이콘 추가 | 강민석 | weather

--- frontend/src/components/Weather/Hourly/SkyIcon.tsx
+++ frontend/src/components/Weather/Hourly/SkyIcon.tsx
@@ -9,11 +9,14 @@ type SkyIconProps = {
 
 const SkyIcon: React.FC<SkyIconProps> = ({ dailySky }) => {
   const SkyStatus = parseInt(dailySky.fcstValue);
-
+  const time = dailySky.fcstTime; // 'fcstTime'은 '0500'과 같은 문자열 형태
+  
   let IconComponent = null;
+  const isNightTime = parseInt(time) >= 1800 || parseInt(time) <= 600 ;
 
   if (SkyStatus >= 0 && SkyStatus <= 5) {
-    IconComponent = <StyledSunFill size={30} />;
+    // 낮 시간대에는 Sun 아이콘, 밤 시간대에는 Moon 아이콘을 렌더링
+    IconComponent = isNightTime ? <StyledMoonFill size={30} /> : <StyledSunFill size={30} />;
   } else if (SkyStatus >= 6 && SkyStatus <= 8) {
     IconComponent = <StyledCloudSunFill size={30} />;
   } else if (SkyStatus >= 9 && SkyStatus <= 10) {
@@ -34,6 +37,11 @@ const StyledSunFill = styled(BsSunFill)`
   margin-top: 5px;
 `;
 
+const StyledMoonFill = styled(BsMoonStarsFill)`
+  color: #6392C7;
+  margin-top: 5px;
+`
+
 const StyledCloudSunFill = styled(BsCloudSunFill)`
   color: #c5c5c5;
   margin-top: 5px;
@@ -43,4 +51,6 @@ const StyledCloudRainFill = styled(BsCloudFill)`
   color: #7a7a7a;
   margin-top: 5px;
 `;
+
+
 export default SkyIcon
