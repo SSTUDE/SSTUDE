d587bd53da42a7dd45bab17c5d58a6eef64036fc
김수찬 <tncks097@naver.com>
2023-11-07 09:52:10 +0900

    feat : 클라이언트에서 제공받을 형태 작업 | 김수찬

---
+++ Backend/busstation/src/main/java/com/sstude/busstation/dto/request/BusGpsRequestDto.java
@@ -0,0 +1,10 @@
+package com.sstude.busstation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+public class BusGpsRequestDto {
+    private String latitude;
+    private String longitude;
+}
