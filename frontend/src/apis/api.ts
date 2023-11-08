5e30b92a030d6958084965950da1b69521b66bb9
강민석 <wwwttt123@naver.com>
2023-11-08 17:56:31 +0900

    ✨ feat : 중기날씨 데이터 API 양식 추가 | 강민석 | weather

--- frontend/src/apis/api.ts
+++ frontend/src/apis/api.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst";
+const BASE_URL = "https://apis.data.go.kr/1360000";
 const SERVICE_KEY = process.env.REACT_APP_WEATHER_API_KEY
 
+// 단기 날씨 정보 요청
 export const getWeatherData = async (params: {
   numOfRows?: number;
   pageNo?: number;
@@ -14,7 +15,7 @@ export const getWeatherData = async (params: {
 }) => {
 
   try {
-    const response = await axios.get(BASE_URL, {
+    const response = await axios.get(`${BASE_URL}/VilageFcstInfoService_2.0/getVilageFcst`, {
       params: {
         serviceKey: SERVICE_KEY,
         ...params
@@ -24,6 +25,50 @@ export const getWeatherData = async (params: {
 
   } catch (error) {
     console.error("API 요청 중 오류가 발생했습니다:", error);
+    throw error;
+  }
+};
+
+// 중기 육상(강수확률, 하늘상태) 예보 API
+export const getMidLandForecast = async (params: {
+  pageNo?: number;
+  numOfRows?: number;
+  dataType?: string;
+  regId: string;
+  tmFc: string;
+}) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/MidFcstInfoService/getMidLandFcst`, {
+      params: {
+        serviceKey: SERVICE_KEY,
+        ...params
+      }
+    });
+    return response.data.response.body.items.item;
+  } catch (error) {
+    console.error("중기 육상 예보 API 요청 중 오류가 발생했습니다:", error);
+    throw error;
+  }
+};
+
+// 중기 기온 정보 API
+export const getMidTemperatureForecast = async (params: {
+  pageNo?: number;
+  numOfRows?: number;
+  dataType?: string;
+  regId: string;
+  tmFc: string;
+}) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/MidFcstInfoService/getMidTa`, {
+      params: {
+        serviceKey: SERVICE_KEY,
+        ...params
+      }
+    });
+    return response.data.response.body.items.item;
+  } catch (error) {
+    console.error("중기 기온 정보 API 요청 중 오류가 발생했습니다:", error);
     throw error;
   }
 };
