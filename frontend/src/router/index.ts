85037cfe205f32f9d20010b5f0e74e4cb7db37a3
강민석 <wwwttt123@naver.com>
2023-11-08 17:25:52 +0900

    🗑️ delete : 불필요한 파일 삭제  | 강민석 | weather

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -11,15 +11,12 @@ import MenuBtn from "../components/Common/MenuBtn";
 import Calender from "../components/Personal/Main/Calender";
 import PreviousPersonalColorResults from "../components/Personal/Previous/PreviousPersonalColorResults";
 import PreviousClothesResults from "../components/Personal/Previous/PreviousClothesResults";
-// import GetWeatherData from "../components/Weather/GetWeatherData";
 import Weather from "../components/Weather/Weather";
 import Bus from "../components/Bus/Bus";
 import BusDetail from "../components/Bus/BusDetail";
 import KakaoMap from "../components/Bus/KakaoMap";
 import BusList from "../components/Bus/BusList";
 import PersonalColorResults from "../components/Personal/Results/PersonalColor/PersonalColorResults";
-import GetWeatherData from "../components/Weather/GetWeatherData";
-
 import SelectContents from "../components/Personal/Capture/SelectContents";
 import PersonalColorCapture from "../components/Personal/Capture/PersonalColorCapture";
 import ClothesCapture from "../components/Personal/Capture/ClothesCapture";
@@ -97,13 +94,10 @@ const routes = [
     path: "/personalloading",
     Component: DiagnosisLoading,
   },
+  // 날씨 정보
   {
     path: "/weather",
     Component: Weather,
-  },
-  {
-    path: "/getweather",
-    Component: GetWeatherData,
   },
 
   // 버스정보
