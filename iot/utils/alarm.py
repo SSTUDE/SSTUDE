242be3ddfef05280aa36584ba149e152789211f5
kimsc9976 <tncks097@naver.com>
2023-11-09 11:15:10 +0900

    delete : delete some files which we dont use

--- iot/utils/alarm.py
+++
@@ -1,4 +0,0 @@
-import pygame
-
-def startAlarm():
-	playsound('/path/to/a/sound/file/you/want/to/play.mp3')
--- iot/websocketServer.py
+++
@@ -1,7 +0,0 @@
-from utils.web_socket import WebSocketService
-from websockets.sync.client import connect 
-        
-
-
-webSocket = WebSocketService(portNo=8765)
-webSocket.start()
