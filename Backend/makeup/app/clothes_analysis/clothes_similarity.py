1dbdca2107e58320eec7e9a53452c7461c51eece
surimjeon <gds0813@naver.com>
2023-11-10 17:10:00 +0900

    🛠 fix :  (fast api) 옷 점수 매기는 로직 수정 | 전수림 | makeup

--- Backend/makeup/app/clothes_analysis/clothes_similarity.py
+++ Backend/makeup/app/clothes_analysis/clothes_similarity.py
@@ -21,28 +21,46 @@ def my_color(userid, current_date, TargetLab):
     
     similarities = []
     i = 0
-    sum = 0
+    # sum = 0
     for idx, mc in enumerate(match_color):
         rgb = sRGBColor(int(mc[0:2], 16), int(mc[2:4], 16), int(mc[4:6], 16), is_upscaled=True)
         lab = convert_color(rgb, LabColor, through_rgb_type=sRGBColor)
         
         #### CIEDE2000 기준으로 차이 계산()
         delta_e = delta_e_cie2000(TargetLab, lab)
-        sum+=delta_e
+        # sum+=delta_e
         if similarities and min(similarities) > delta_e:
             i = idx
         
         similarities.append(delta_e)
+        
+    ## 차이가 많이나면,(즉 평균이 높으면 -> 높을수록 )
     
-    print(sum/len(match_color), sum)
+    # print(sum/len(match_color), sum)
     print(i, min(similarities), match_color[i])
     score = 100-int(min(similarities))
-    print(score)
+    print(final_score(score)) 
+    # print(score)
+    # print(final_score(100-sum/len(match_color)))
     
     
-    return score
-
+    return final_score(score)
 
 
 # 어울리는 점수 도출하기 
 # 그 색상들과 유사도를 측정해서 점수 메기기 
+
+def final_score(score):
+    if score>=90:
+        return score
+    elif score>=80:
+        return score*0.9
+    elif score>=70:
+        return score*0.8
+    elif score>=60:
+        return score*0.7
+    elif score>=50:
+        return score*0.6
+    else:
+        return score*0.5
+    
