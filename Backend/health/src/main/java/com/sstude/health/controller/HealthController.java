8bbd8fcf7c9a5605b5eac9ee733dd562855928f5
최혜원 <hwchoi33@naver.com>
2023-11-10 17:25:14 +0900

    💯 test :  배포 테스트  | 최혜원 | health

--- Backend/health/src/main/java/com/sstude/health/controller/HealthController.java
+++ Backend/health/src/main/java/com/sstude/health/controller/HealthController.java
@@ -89,4 +89,13 @@ public class HealthController {
         return ResponseEntity.ok(response);
     }
 
+    @GetMapping("/t")
+    public String hey(@RequestHeader("Authorization") @Parameter(hidden = true) final String token){
+        return "t!";
+    }
+
+    @GetMapping("/t2")
+    public String hey2(@RequestHeader("Authorization") @Parameter(hidden = true) final String token){
+        return token;
+    }
 }
