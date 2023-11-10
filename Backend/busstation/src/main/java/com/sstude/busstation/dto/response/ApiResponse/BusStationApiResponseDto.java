74a95810f83310201289aa8659e0124d34456af1
김수찬 <tncks097@naver.com>
2023-11-10 10:12:28 +0900

    feat : Openapi response 파일 추가 및 위치 이동

---
+++ Backend/busstation/src/main/java/com/sstude/busstation/dto/response/ApiResponse/BusStationApiResponseDto.java
@@ -0,0 +1,40 @@
+package com.sstude.busstation.dto.response.ApiResponse;
+
+import com.sstude.busstation.dto.response.BusStationResponseDto;
+import com.sstude.busstation.utils.ApiResponseDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BusStationApiResponseDto implements ApiResponseDto<BusStationApiResponseDto, BusStationResponseDto> {
+    private Integer citycode;
+    private String gpslati;
+    private String gpslong;
+    private String nodeid;
+    private String nodenm;
+    private Integer nodeno;
+
+    @Override
+    public List<BusStationApiResponseDto> _getItems() {
+        return null;
+    }
+
+    @Override
+    public BusStationResponseDto of(BusStationApiResponseDto item) {
+        return BusStationResponseDto.builder()
+                .cityCode(item.getCitycode())
+                .latitude(item.getGpslati())
+                .longitude(item.getGpslong())
+                .nodeName(item.getNodenm())
+                .nodeId(item.getNodeid())
+                .nodeNo(item.getNodeno())
+                .build();
+    }
+}
