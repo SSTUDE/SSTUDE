cecaafb41aa5f8ea3446b3d64b82c204d8ef912a
김수찬 <tncks097@naver.com>
2023-11-11 18:45:25 +0900

    feat: 정보 저장용 함수 생성 | 김수찬

--- Backend/busstation/src/main/java/com/sstude/busstation/dto/response/BusInformByStationResponseDto.java
+++ Backend/busstation/src/main/java/com/sstude/busstation/dto/response/BusInformByStationResponseDto.java
@@ -1,5 +1,6 @@
 package com.sstude.busstation.dto.response;
 
+import com.sstude.busstation.entity.Bus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,4 +17,14 @@ public class BusInformByStationResponseDto {
 
     private String startNodeNum;
     private String endNodeNum;
+
+    public static BusInformByStationResponseDto toDto(Bus dto){
+        return BusInformByStationResponseDto.builder()
+                .routeId(dto.getRouteId())
+                .routeNo(dto.getRouteNo())
+                .routeType(dto.getRouteType())
+                .startNodeNum(dto.getStartNodeNum())
+                .endNodeNum(dto.getEndNodeNum())
+                .build();
+    }
 }
