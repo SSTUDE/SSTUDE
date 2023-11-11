006325c6822293c5648c697dc25cbb287ea757d3
김수찬 <tncks097@naver.com>
2023-11-11 18:46:52 +0900

    feat : DB 조회용 JPA 생성

--- Backend/busstation/src/main/java/com/sstude/busstation/repository/BusRepository.java
+++ Backend/busstation/src/main/java/com/sstude/busstation/repository/BusRepository.java
@@ -4,6 +4,9 @@ import com.sstude.busstation.entity.Bus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BusRepository extends JpaRepository<Bus, String> {
+    List<Bus> findBusesByMemberId(Long memberId);
 }
