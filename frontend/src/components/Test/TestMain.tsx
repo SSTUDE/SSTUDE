88868627045bf7e28f018663092c47cc0cfa8bf8
강민석 <wwwttt123@naver.com>
2023-11-14 22:08:46 +0900

    ✨ feat : 클릭시 소리 구현 | 강민석 | App

--- frontend/src/components/Test/TestMain.tsx
+++ frontend/src/components/Test/TestMain.tsx
@@ -1,23 +1,39 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import { findNearestSFGridItem } from '../Weather/Grid';
 
 const TestMain = () => {
-  const handleButtonClick = () => {
-    // 예시 위도와 경도
-    const latitude = 36.10774883999876;
-    const longitude = 128.4220535380925;
+  // 오디오 요소에 대한 참조 생성
+  const audioRef = useRef<HTMLAudioElement>(null);
 
-    const nearestItem = findNearestSFGridItem(latitude, longitude);
-    console.log(nearestItem);
-  };
+  useEffect(() => {
+    // 클릭 이벤트 핸들러
+    const playSound = () => {
+      if (audioRef.current) {
+        audioRef.current.play();
+      }
+    };
+
+    // 전역 클릭 이벤트 리스너 추가
+    window.addEventListener('click', playSound);
+
+    // 컴포넌트 언마운트 시 이벤트 리스너 제거
+    return () => {
+      window.removeEventListener('click', playSound);
+    };
+  }, []);
 
   return (
     <div>
-      <button onClick={handleButtonClick}>가장 가까운 위치 찾기</button>
+      <audio src="/assets/sounds/blop.mp3" ref={audioRef}></audio>
     </div>
   );
 };
+
+const BTN = styled.button`
+  width: 300px;
+  height: 300px;
+`
 
 export default TestMain
