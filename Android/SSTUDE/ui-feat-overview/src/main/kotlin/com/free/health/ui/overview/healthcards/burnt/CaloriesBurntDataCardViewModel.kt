a9abe92bae1a3708d7738ae713c57c8dd6087a70
최혜원 <hwchoi33@naver.com>
2023-11-15 09:46:11 +0900

    ✨ feat : 안드로이드 로직 완성 시간 수정 | 최혜원 | android

---
+++ Android/SSTUDE/ui-feat-overview/src/main/kotlin/com/free/health/ui/overview/healthcards/burnt/CaloriesBurntDataCardViewModel.kt
@@ -0,0 +1,115 @@
+package com.free.health.ui.overview.healthcards.burnt
+
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.free.health.domain.base.getDateTimeAtEndOfDay
+import com.free.health.domain.base.getDateTimeAtStartOfDay
+import com.free.health.domain.healthdata.HealthDataCalories
+import com.free.health.domain.healthdata.models.CaloriesConsumed
+import com.free.health.ui.overview.healthcards.OverviewCardUiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import java.time.LocalDate
+import javax.inject.Inject
+
+@HiltViewModel
+class CaloriesBurntDataCardViewModel @Inject constructor(
+    private val caloriesDataCalories: HealthDataCalories
+) : ViewModel() {
+
+    var uiState by mutableStateOf<OverviewCardUiState>(OverviewCardUiState.Loading)
+        private set
+
+    fun setInitialDate(date: LocalDate) {
+        viewModelScope.launch {
+            caloriesDataCalories
+                .readBurnt(
+                    from = date.getDateTimeAtStartOfDay(),
+                    until = date.getDateTimeAtEndOfDay(),
+                )
+                .onSuccess(::onCaloriesDataReturned)
+                .onFailure(::onCaloriesDataFailureReturned)
+        }
+    }
+
+
+    private fun onCaloriesDataFailureReturned(throwable: Throwable) {
+        Log.e(LOG_TAG, throwable.message ?: "Error")
+        uiState = OverviewCardUiState.Error
+    }
+
+//    private fun onCaloriesDataReturned(calories: Int?) {
+//        uiState = if (calories == null) {
+//            OverviewCardUiState.NoData
+//        } else {
+//            OverviewCardUiState.Loaded(
+//                amount = "$calories",
+//                type = "kcals"
+//            )
+//        }
+//    }
+
+
+
+//    private fun onCaloriesDataReturned(steps: Int?) {
+//        uiState = if (steps == null) {
+//            OverviewCardUiState.NoData
+//        } else {
+//            val calculatedValue = steps * 0.0408443789887089
+//            Log.d("CaloriesData", "Calculated Value: $calculatedValue")
+//            val roundedValue = String.format("%.1f", calculatedValue)
+//            Log.d("CaloriesData", "Rounded Value: $roundedValue")
+//            OverviewCardUiState.Loaded(
+//                amount = "$roundedValue*",
+//                type = "kcals"
+//            )
+//        }
+//    }
+
+    private fun onCaloriesDataReturned(steps: Int?) {
+        uiState = if (steps == null) {
+            OverviewCardUiState.NoData
+        } else {
+            val calculatedValue = steps * 0.0277
+            val roundedValue = String.format("%.0f", calculatedValue)
+            OverviewCardUiState.Loaded(
+                amount = roundedValue,
+                type = "steps"
+            )
+        }
+    }
+
+//    private fun onCaloriesDataReturned(steps: Int?) {
+//        uiState = if (steps == null) {
+//            OverviewCardUiState.NoData
+//        } else {
+//            OverviewCardUiState.Loaded(
+//                amount = "$steps*",
+//                type = "steps"
+//            )
+//        }
+//    }
+
+//    private fun onCaloriesDataFailureReturned(throwable: Throwable) {
+//        Log.e(LOG_TAG, throwable.message ?: "Error")
+//        uiState = OverviewCardUiState.Error
+//    }
+//
+//    private fun onCaloriesDataReturned(calories: CaloriesConsumed) {
+//        uiState = if (calories.burnt == 0) {
+//            OverviewCardUiState.NoData
+//        } else {
+//            OverviewCardUiState.Loaded(
+//                amount = calories.burnt.toString(),
+//                type = "kcals"
+//            )
+//        }
+//    }
+    companion object {
+        private val LOG_TAG = CaloriesBurntDataCardViewModel::class.java.simpleName
+    }
+}
