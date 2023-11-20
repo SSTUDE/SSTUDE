5a1852e6d7299694237d08a7caa6884b936ad028
서지호 <cjsrhd882@naver.com>
2023-11-21 03:20:49 +0900

    ✨ feat : 알람 웹소켓 적용 | 서지호 | Alarm

--- frontend/src/components/Common/Alarm.tsx
+++ frontend/src/components/Common/Alarm.tsx
@@ -1,13 +1,15 @@
 import * as React from "react";
 import dayjs from "dayjs";
-import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { StaticTimePicker } from "@mui/x-date-pickers/StaticTimePicker";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { koKR } from "@mui/x-date-pickers/locales";
-import { GlobalStyles } from "@mui/system";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import { useWebSocket } from "../../hooks/useWebSocket";
+import { RASPBERRY_URL } from "../../apis/constants";
+import { useCustomAlert } from "../../hooks/useAlert";
 
 const theme = createTheme(
   {
@@ -93,7 +95,7 @@ const TimePickerWrapper = styled.div`
   .MuiTimeClock-root {
     margin: auto;
     /* width: 700px; */
-    /* height: 700ox; */
+    /* height: 700px; */
     /* max-height: 1000px; */
     margin-top: 5vh;
   }
@@ -110,6 +112,12 @@ const TimePickerWrapper = styled.div`
   // 시계 전체 컨테이너
   .MuiPickersLayout-contentWrapper {
     transform: scale(3);
+  }  
+  // 시계 터치영역 조절 (100/배율)%
+  .MuiClock-squareMask {
+    /* left: 33.333%; */
+    /* top: 33.333%; */
+    /* background-color: red; */
   }
   // 시계 시간 및 분 선택 안 되었을 때
   .MuiClockNumber-root {
@@ -136,27 +144,90 @@ const TimePickerWrapper = styled.div`
     left: calc(50% - 15px);
     top: -18px;
   }
-  // 취소 확인 버튼 DIV
-  .MuiDialogActions-root.MuiDialogActions-spacing.MuiPickersLayout-actionBar {
-    position: relative;
-    bottom: 50px;
-    left: 400px;
-    padding: 0;
-  }
-  // 취소 확인 버튼
-  .MuiButtonBase-root.MuiButton-root.MuiButton-text.MuiButton-textPrimary.MuiButton-sizeMedium.MuiButton-textSizeMedium.MuiButton-root.MuiButton-text.MuiButton-textPrimary.MuiButton-sizeMedium.MuiButton-textSizeMedium {
-    font-size: 2.5rem;
-    color: white;
-    font-family: "Giants-Bold";
-  }
+`;
+
+const SelectTimeBg = styled.div`
+  position: fixed;
+  right: 12.6%; 
+  bottom: 4.2%; 
+  padding: 10px;
+  border-radius: 5px;
+  cursor: pointer;
+`
+
+const UnSelectTimeBg = styled.div`
+  position: fixed;
+  right: 18.4%; 
+  bottom: 4.2%; 
+  padding: 10px;
+  border-radius: 5px;
+  cursor: pointer;
+`
+
+const SelectTime = styled.p`
+  font-size: 2.5rem;
+  color: white;
+  font-family: "Giants-Bold";
+  padding: 10px;
 `;
 
 export default function Alarm() {
+  // const { sendMessage } = useWebSocket(RASPBERRY_URL);
+  const showAlert = useCustomAlert();
+  const [selectTime, setSelectTime] = React.useState(dayjs());
+
+  const timeChange = (newTime: any) => {
+    setSelectTime(newTime);
+  };
+
+  const navigate = useNavigate();
+
+  const HandleUnSelect = () => {
+    navigate('/mirror');
+  };
+
+  const HandleSelect = () => {
+    const hour = selectTime.hour();
+    const minute = selectTime.minute();
+
+    const message = {
+      start_hour: hour,
+      start_minute: minute,
+      play_times: 1,
+      duration_minutes: 1
+    };
+
+    console.log(message)
+
+    // sendMessage(message)
+    //   .then((response) => {
+    showAlert({
+      icon: 'success',
+      title: '알람이 설정 되었습니다.',
+      timer: 1500,
+    });
+    navigate('/mirror');
+    // })
+    // .catch((error) => {
+    //   console.log(error);
+    // });
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <TimePickerWrapper>
-          <StaticTimePicker defaultValue={dayjs()} />
+          <StaticTimePicker
+            displayStaticWrapperAs="mobile"
+            value={selectTime}
+            onChange={timeChange}
+          />
+          <UnSelectTimeBg onClick={HandleUnSelect}>
+            <SelectTime>취소</SelectTime>
+          </UnSelectTimeBg>
+          <SelectTimeBg onClick={HandleSelect}>
+            <SelectTime>확인</SelectTime>
+          </SelectTimeBg>
         </TimePickerWrapper>
       </LocalizationProvider>
     </ThemeProvider>
