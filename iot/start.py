e6cea0ea98b921b66624eb0700e640c6f43eb29a
김수찬 <tncks097@naver.com>
2023-11-21 09:44:40 +0900

    update : 테스트를 위한 ctrl+c 인식용 코드 추가

--- iot/start.py
+++ iot/start.py
@@ -1,5 +1,6 @@
 import multiprocessing
 import os
+import signal
 
 def start_camera_service():
     os.system('python camera.py')
@@ -7,7 +8,15 @@ def start_camera_service():
 def start_alarm_service():
     os.system('python alarm.py')
 
+def signal_handler(signum, frame):
+    camera_process.terminate()
+    alarm_process.terminate()
+    print("프로세스가 종료되었습니다.")
+
 if __name__ == '__main__':
+    # 시그널 핸들러 등록
+    signal.signal(signal.SIGINT, signal_handler)
+
     # 프로세스 생성
     camera_process = multiprocessing.Process(target=start_camera_service)
     alarm_process = multiprocessing.Process(target=start_alarm_service)
